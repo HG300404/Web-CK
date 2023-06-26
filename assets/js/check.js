afabfc9773059c418334d42b495c99052b12903a
HG300404 <131573387+HG300404@users.noreply.github.com>
2023-06-26 22:16:10 +0700

    Update check.js

--- assets/js/check.js
+++ assets/js/check.js
@@ -6,38 +6,23 @@ function Login() {
   // Kiểm tra nếu tên đăng nhập và mật khẩu hợp lệ
   if (email === "nglienhg000@gmail.com" && password === "012") {
     // Nếu hợp lệ, chuyển hướng sang trang khác
-    alert("đăng nhập thành công!");
+    alert("Login Success!");
     window.location.href = "infor.html";
   } else {
     // Nếu không hợp lệ, hiển thị thông báo lỗi
-    alert("Tên đăng nhập hoặc mật khẩu không đúng!");
-  }
-}
-function Login() {
-  //   event.preventDefault(); // Ngăn chặn form submit mặc định của trình duyệt
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("pw").value;
-
-  // Kiểm tra nếu tên đăng nhập và mật khẩu hợp lệ
-  if (email === "nglienhg000@gmail.com" && password === "012") {
-    // Nếu hợp lệ, chuyển hướng sang trang khác
-    alert("đăng nhập thành công!");
-    window.location.href = "infor.html";
-  } else {
-    // Nếu không hợp lệ, hiển thị thông báo lỗi
-    alert("Tên đăng nhập hoặc mật khẩu không đúng!");
+    alert("Email or password is incorrect!");
   }
 }
 
 function Signup() {
+  // var email_s = document.getElementById("email_s").value;
   var password_s = document.getElementById("password_s").value;
   var confirm_s = document.getElementById("confirm-password_s").value;
 
-  if (password_s === confirm_s) {
-    alert("Đăng ký thành công!");
-    window.location.href = "login.html";
+  if (password_s != confirm_s) {
+    alert("Confirmation password is incorrect!");
   } else {
-    alert("Mật khẩu xác nhận không chính xác!");
+    alert("Signup Success");
+    window.location.href = "login.html";
   }
 }
-
